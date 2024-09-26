752978760696980e9b8e432b3576bb5aeb317f1e
bruford8 <ap2545773@gmail.com>
2024-09-26 12:16:39 +0900

    Delete hhhsd.py

--- hhhsd.py
+++
@@ -1,9 +0,0 @@
-name_film = input("Введите название фильма")
-name_cinema = input("Введите название кинотеатра")
-time = input("Введите время")
-print(f'Билет на " {name_film} " в " {name_cinema} " на " {time} забронирован ')
-
-monthly_salary = int(input("Введите зарплату за месяц"))
-worked_hours = int(input("Введите количество отработанных часов в выходные"))
-award = monthly_salary * 0.01 * worked_hours
-print("Размер премии:", award)
