2ccbc016b1d47d1441e6527a28ed21d777baaa58
bruford8 <ap2545773@gmail.com>
2025-05-15 12:11:14 +0900

    Update ааа.py

--- ааа.py
+++ ааа.py
@@ -1,56 +1,28 @@
 class Ship:
-    def __init__(self):
-        self.size = 5
-        self.hp = 5
-        self.status = True
-
-    pass
-
-class Battleship(Ship):
-    def __init__(self):
-        super().__init__()
-        self.size = 10
-        self.hp = 10
-        self.status = True
-
-    pass
-
-class Cruiser(Ship):
-    def __init__(self):
-        super().__init__()
-        self.size = 8
-        self.hp = 8
-        self.status = True
-
-    pass
-
-class TheDestroyer(Ship):
-    def __init__(self):
-        super().__init__()
-        self.size = 6
-        self.hp = 6
-        self.status = True
-
-    pass
-
-class Boat(Ship):
-    def __init__(self):
-        super().__init__()
-        self.size = 3
-        self.hp = 3
-        self.status = True
-
-    pass
-
-class Player1:
-    def __init__(self, name):
+    def __init__(self, name, hp):
         self.name = name
+        self.hp = hp
+        self.status = True
 
-    def fire(self):
 
-class Player2:
-    def __init__(self, name):
+class Player:
+    def __init__(self, name, color):
         self.name = name
+        self.color = color
 
-    def fire(self):
+    def attack(self):
+        pass
 
+    def info(self):
+        print(f'Имя: {self.name}\n'
+              f'Цвет команды: {self.color}')
+
+
+class Playing_field:
+    def __init__(self, name, height, width):
+        self.name = name
+        self.height = height
+        self.width = width
+
+    def clear(self):
+        pass
